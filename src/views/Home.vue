9dbae99a4c0930ff937361514bdc9440e5433038
Luis David <luis.galicia@dit.mx>
2021-08-27 21:31:13 -0500

    Se agregó la card de arcup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
-    <HelloWorld/>
+    <div class="ma-8">
+      <CCard/>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from '../components/HelloWorld.vue';
+import CCard from '../components/CCard.vue';
 
 export default {
   name: 'Home',
   
   components: {
     HelloWorld,
+    CCard,
   },
 };
 </script>
