6938c7633256a8ffd1a1ea5027639fb79acec134
Jesús Eduardo Hernández Martínez <jesus_hdezmtz@hotmail.com>
2021-08-28 13:30:29 -0500

    Test depploy

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          <div>Welcome to the Vuetify 3 Alpha</div>
+          <div>Welcome to the Vuetify 3 Alpha Test</div>
         </h1>
 
 
