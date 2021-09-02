0203bfcf1bfe1510bda5b03e44c0057acd928aa6
Luis David <luis.galicia@dit.mx>
2021-09-01 21:57:50 -0500

    Se agregó TheNavigation & CCard component

--- src/components/layouts/TheNavigation.vue
+++ src/components/layouts/TheNavigation.vue
@@ -1,74 +1,19 @@
 <template>
-  <v-card
-    class="mx-auto"
-    height="300"
-    width="330"
-  >
-    <v-navigation-drawer
-      permanent
-      width="100%"
-    >
-      <v-row
-        class="fill-height"
-        no-gutters
-      >
-        <v-navigation-drawer
-          dark
-          mini-variant
-          mini-variant-width="56"
-          permanent
-        >
-          <v-list-item class="px-2">
-            <v-list-item-avatar>
-              <v-img src="https://randomuser.me/api/portraits/women/75.jpg"></v-img>
-            </v-list-item-avatar>
-          </v-list-item>
-
-          <v-divider></v-divider>
-
-          <v-list
-            dense
-            nav
-          >
-            <v-list-item
-              v-for="item in items"
-              :key="item.title"
-            >
-              <v-list-item-action>
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-list-item-action>
-
-              <v-list-item-content>
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-navigation-drawer>
-
-        <v-list class="grow">
-          <v-list-item
-            v-for="link in links"
-            :key="link"
-            link
-          >
-            <v-list-item-title v-text="link"></v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-row>
-    </v-navigation-drawer>
-  </v-card>
+  <div>
+    <div class="content">
+      <p>Hola, estoy dentro del contenido</p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-    name: 'TheNavigation',
-    data: () => ({
-        items: [
-          { title: 'Home', icon: 'mdi-view-dashboard' },
-          { title: 'About', icon: 'mdi-forum' },
-        ],
-        links: ['Home', 'Contacts', 'Settings'],
-        mini: true,
-    })
-}
+export default {};
 </script>
+<style lang="sass" scoped>
+.content
+  height: 100%
+  width: 25%
+  position: fixed
+  border: 1px solid red
+  z-index: 10
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,30 @@
 <template>
   <div>
-    <div class="ma-8">
-      <CCard/>
+    <TheNavigation />
+    <div id="container" class="container">
+      <CCard />
     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld.vue';
-import CCard from '../components/CCard.vue';
+import HelloWorld from "../components/HelloWorld.vue";
+import CCard from "../components/CCard.vue";
+import TheNavigation from "../components/layouts/TheNavigation.vue";
 
 export default {
-  name: 'Home',
-  
+  name: "Home",
+
   components: {
     HelloWorld,
     CCard,
+    TheNavigation,
   },
 };
 </script>
+
+<style lang="sass" scoped>
+.container
+  position: relative
+  margin-left: 25%
+</style>
