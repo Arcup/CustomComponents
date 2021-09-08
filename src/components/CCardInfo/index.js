c757d5a87aa6373c8742538286ce1f96b33066fe
Luis David <luis.galicia@dit.mx>
2021-09-07 22:20:53 -0500

    Se agregaron las carpetas de la nueva card

---
+++ src/components/CCardInfo/index.js
@@ -0,0 +1,43 @@
+export const style = {
+    "card" : {
+        "position": "relative",
+        "width": "300px",
+        "height": "500px",
+        "border-radius": "3px",
+        "border": "1px",
+        "border-style": "solid",
+        "border-color": "#73f3f3ff",
+        "background-color": "#f8f8f8ff",
+        "text-align": "center"
+    },
+
+    "header": {
+        "width": "100%",
+        "height": "166px",
+        "top": "0",
+        "background-color": "#ffffffff"
+    }, 
+
+    "body": {
+        "height" : "auto"
+    },
+
+    "button": {
+        "position": "absolute",
+        "width": "90%",
+        "height": "auto",
+        "margin-bottom": "5px",
+        "border-radius": "3px",
+        "margin-left": "5%",
+        "bottom": "0",
+        "color": "white",
+        "background-color": "#161e2eff",
+        "cursor": "pointer"
+    }
+}
+
+export const data = {
+    "title" : "Esto es el header",
+    "body" : "Soy el lorem del body",
+    "button" : "Aceptar"
+}
