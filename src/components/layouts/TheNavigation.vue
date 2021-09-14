67cb33dd8359e489038a79f1df828c0109aa2f64
Luis David <luis.galicia@dit.mx>
2021-09-13 23:28:07 -0500

    Se creó el sidebar y su estructura

--- src/components/layouts/TheNavigation.vue
+++ src/components/layouts/TheNavigation.vue
@@ -1,19 +1,74 @@
 <template>
   <div>
     <div class="content">
-      <p>Hola, estoy dentro del contenido</p>
+      <section>
+        <h1>Cartmanmundi</h1>
+        <img
+          v-bind:src="require('./../../assets/cartmangordo.jpg')"
+          alt=""
+          class="section__image"
+        />
+      </section>
+      <hr />
+      <ul>
+        <li class="list__element-first">
+          Card componentes
+          <ul>
+            <li class="list__element-second" v-for="card in cardList" :key="card.nombre">{{ card.nombre }}</li>
+          </ul>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { reactive } from "vue";
+
+export default {
+  setup(props, { emit }) {
+    const cardList = reactive(
+      {
+        nombre: "Card Information",
+        component: "",
+      },
+      {
+        nombre: "Card Low",
+        component: "",
+      }
+    );
+
+    const clicNavigation = () => {
+      emit("clicNavigation");
+    };
+
+    return {
+      cardList,
+      clicNavigation,
+    };
+  },
+};
 </script>
 <style lang="sass" scoped>
 .content
   height: 100%
-  width: 25%
+  width: 20%
   position: fixed
-  border: 1px solid red
+  box-shadow: 0 2px 1px -1px rgba(0,0,0,.2),0 1px 1px 0 rgba(0,0,0,.14),0 1px 3px 0 rgba(0,0,0,.12)!important
   z-index: 10
+
+.list__element-first
+  margin-left: 2px
+  font-size: 14pt
+  font-family: bold
+
+.list__element-second
+  margin-left: 4px
+  font-size: 10pt
+  cursor: pointer
+
+.section__image
+  width: 120px
+  height: 70px
+  margin-left: 15px
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,20 @@
 <template>
   <div>
-    <TheNavigation />
+    <TheNavigation @clicNavigation="getComponent()"/>
     <div id="container" class="container">
-      <CCardInfo />
+      <CCardInfo @clicButton="soyUnError()"/>
     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld.vue";
-import CCard from "../components/CCard.vue";
+import HelloWorld from "../components/HelloWorld.vue"
+import CCard from "../components/CCard.vue"
 import CCardInfo from "../components/CCardInfo/CCardInfo.vue"
-import TheNavigation from "../components/layouts/TheNavigation.vue";
+import CCardLow from "../components/CCardLow/CCardLow.vue"
+import TheNavigation from "../components/layouts/TheNavigation.vue"
 
-import { ref, reactive } from "vue";
+import { ref, reactive } from "vue"
 
 export default {
   name: "Home",
@@ -22,14 +23,38 @@ export default {
     HelloWorld,
     CCard,
     CCardInfo,
+    CCardLow,
     TheNavigation,
   },
   setup(props) {
     const state = reactive({
       color: "red",
     })
+    const soyUnError = () => {
+
+      try {
+         let sn = ['teclado', 'mouse', 'pantalla'];
+        /*sn.forEach((e, i, a) => {
+          console.log('indice: ' + i + '-' + 'elemento: ' + e + ', content: ' + a);
+        })
+        var namelistA = new Array(4294967296);*/
+        let holi = sn.splice(0, 2);
+        console.log('Solo me falta mi pantalla porque yo soy: ' + holi);
+       
+      } catch (e) {
+        if (e instanceof RangeError) {
+          console.log('Entro a la excepción que queríamos');
+        }
+        console.log('lo cachamos' + e);
+      }
+    };
+    const evento = () => {
+      console.log("Estoy desde el padre");
+    };
     return {
       state,
+      evento,
+      soyUnError
     }
   }
 };
@@ -38,5 +63,5 @@ export default {
 <style lang="sass" scoped>
 .container
   position: relative
-  margin-left: 25%
+  margin-left: 22%
 </style>
