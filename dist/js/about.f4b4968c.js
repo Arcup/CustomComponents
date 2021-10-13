f6ec2f8a57d27c4a0bcf833340e79c2c725ab974
Jesús Eduardo Hernández Martínez <jesus_hdezmtz@hotmail.com>
2021-10-12 19:25:15 -0500

    Remove carpeta dist

--- dist/js/about.f4b4968c.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var c=a("7a23");const e={class:"about"},o=Object(c["h"])("h1",null,"This is an about page",-1),s=[o];function u(n,t){return Object(c["w"])(),Object(c["g"])("div",e,s)}var b=a("6b0d"),i=a.n(b);const r={},w=i()(r,[["render",u]]);t["default"]=w}}]);
-//# sourceMappingURL=about.f4b4968c.js.map
